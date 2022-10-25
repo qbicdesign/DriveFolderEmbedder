623fa182a8898df3f5e3ffe0f7370451797b36f7
qbicdesign <qbicdesign@gmail.com>
2022-10-25 22:55:09 +0300

    Update driveFolderEmbedder.php
    Made it a bit prettier
    displaying filenames as hyperlinked <li> items
    View link to the right has been removed.
    
    Also suggest adding this to your custom CSS, for some nice rounded corners on the tabs
    .sow-tabs-tab, sow-tabs-tab-selected {
    border-radius: 5px 5px 0px 0px;
    }
    
    Code could be better modded so there isn't a table at all.  Tables are kinda old school

--- drive-folder-embedder/driveFolderEmbedder.php
+++ drive-folder-embedder/driveFolderEmbedder.php
@@ -2,7 +2,7 @@
     /*
         Plugin Name: Drive Folder Embedder
         Plugin URI: https://github.com/azumbro/DriveFolderEmbedder
-        Version: 1.0.2
+        Version: 1.1.0
         Description: A Wordpress plugin that dynamically creates a table with file names and links for a shared Google Drive folder.
         Author: azumbro
     */
@@ -20,10 +20,12 @@
         $htmlStr = str_replace("script", "", (addslashes (file_get_contents($baseURL . $folderid))));
         $tableID = rand();
         return "
+
             <table id='driveFolderEmbedder-" . $tableID .  "' class='". $tablecssclass ."'>"
-                . ($showheader == "True" ? "<thead><td>File Name</td><td></td></thead>" : "") .
+                . ($showheader == "True" ? "<thead><td>File Name</td></thead>" : "") .
                 "<tbody></tbody>
             </table>
+
             <script>
                 var sortMethod = '" . $sort . "';
                 var sortFunctions = {
@@ -47,20 +49,25 @@
                         });
                     }
                 };
-                var sortFunction = sortFunctions[sortMethod];
-                var parsedEntries = [];
-                var html = jQuery.parseHTML( '". $htmlStr ."');
-                var entries = jQuery(html).find('.flip-entry-info'); 
-                jQuery.each(entries, function(index, value) { 
-                    parsedEntries.push({ 
-                        name: jQuery(value)[0].innerText,
-                        url: jQuery(value).find('a').slice(0, 1).attr('href')
-                    }); 
-                }); 
-                parsedEntries.sort(sortFunction);
-                jQuery.each(parsedEntries, function(index, value) {
-                    jQuery('#driveFolderEmbedder-" . $tableID .  " tbody').append('<tr><td>' + value.name + '</td><td><a href=\"' + value.url + '\" target=\"_blank\">View</a></td></tr>');
-                });
+                var sortFunction = sortFunctions[sortMethod];                                                                                                                 
+                var parsedEntries = [];                                                                                                                                                                                                                                                                                      
+                var htmlStr = '". $htmlStr ."';                                                                                                                                     
+                var tempEl = document.createElement('div');                                                                                                                     
+                tempEl.innerHTML = htmlStr;                                                                                                                                           
+                var entries = tempEl.getElementsByClassName('flip-entry-info');                                                                                                                                                                                                                                               
+                for(var entry of entries) {                                                                                                                                     
+                    parsedEntries.push({                                                                                                                                  
+                        name: entry.innerText,                                                                                                                            
+                        url: entry.innerHTML.split('\"')[1]                                                                                                               
+                    });                                                                                                                                                    
+                }                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                parsedEntries.sort(sortFunction);                                                                                                                                                                                                                                                                                                           
+                var tableEl = document.getElementById('driveFolderEmbedder-" . $tableID . "').getElementsByTagName('tbody')[0];                                                                                                                                                                                              
+                for(var entry of parsedEntries) {                                                                                                                           
+                    var tr = document.createElement('tr');                                                                                                                
+                    tr.innerHTML = '<li><a href=\"' + entry.url + '\" target=\"_blank\">' + entry.name + '</a></li>';                                                                                                                                                                 
+                    tableEl.appendChild(tr);                                                                                                                                   
+                }                                                                                                                                                             
             </script>
         ";
     }
